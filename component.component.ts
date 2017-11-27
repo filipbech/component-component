69dccd2480f8de30f1ae24b5ead47a214ff75f81
Søren Bruus Frank <soerenbf@gmail.com>
2017-11-27 13:51:35 +0100

    Dynamic outputs added to component.

--- component.component.ts
+++ component.component.ts
@@ -5,36 +5,60 @@ import {
     Input, 
     OnChanges,
     ChangeDetectionStrategy, 
-    ComponentRef } from '@angular/core';
+    ComponentRef,
+    EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'component',
     template: ' ' /* Cannot be empty */
 })
-export class ComponentComponent implements OnChanges {
-    @Input() 
-    private component;
+export class ComponentComponent implements OnChanges, OnDestroy {
+    @Input() private component;
 
-    @Input() 
-    private inputs: { [name:string]: any } = {};
+    @Input() private inputs: { [name: string]: any } = {};
+    @Input() private outputs?: { [name: string]: any };
     
+    private subscriptions: { [name: string]: Subscription } = {};
     private _component: ComponentRef<any>;
 
 	constructor(
 		private _vcRef: ViewContainerRef, 
 		private _cfResolver: ComponentFactoryResolver
-	) { }
+    ) { }
 
 	ngOnChanges(changes) {
-        if(changes['component']) {
+        if(changes['component'].currentValue) {
             this._vcRef.clear();
             const cf = this._cfResolver.resolveComponentFactory<any>(this.component);
             this._component = this._vcRef.createComponent(cf);
         }
-        if(changes['inputs'] || changes['component']) {
+
+        if(changes['inputs'].currentValue || changes['component'].currentValue) {
             Object.keys(this.inputs).forEach(key => {
                 this._component.instance[key] = this.inputs[key];
             });
         }
-	}
+
+        if((changes['outputs'] && changes['outputs'].currentValue && this._component) || (changes['component'] && changes['component'].currentValue && this.outputs)) {
+            const newOutputs = Object.keys(this.outputs).filter(key => !this.subscriptions[key]);
+            const removedOutputs = Object.keys(this.subscriptions).filter(key => !this.outputs[key]);
+
+            newOutputs.forEach(key => {
+                if (this._component.instance.hasOwnProperty(key) && this._component.instance[key] instanceof EventEmitter) {
+                    this.subscriptions[key] = this._component.instance[key].subscribe(data =>{
+                        this.outputs[key](data);
+                    });
+                }
+            });
+
+            removedOutputs.forEach(key => {
+                this.subscriptions[key].unsubscribe();
+                delete this.subscriptions[key];
+            })
+        }
+    }
+    
+    ngOnDestroy() {
+        Object.keys(this.subscriptions).forEach(key => this.subscriptions[key].unsubscribe());
+    }
 }
